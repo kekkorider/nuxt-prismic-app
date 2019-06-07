732c5e17eae305ee60ae00ade9a027f565202f66
Jack-Barry <jackaldenbarry@gmail.com>
2019-06-06 21:44:13 -0500

    Fix for broken routing on Netlify deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,78 +1,76 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
-const pkg = require('./package')
-const Prismic = require('prismic-javascript')
-import { initApi, queryForDocType } from './prismic.config'
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+const pkg = require("./package");
+const Prismic = require("prismic-javascript");
+import { initApi, queryForDocType } from "./prismic.config";
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
+
+  env: {
+    PRISMIC_API_URL: process.env.PRISMIC_API_URL,
+    PRISMIC_API_KEY: process.env.PRISMIC_API_KEY
+  },
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/css/global.css'
-  ],
+   ** Global CSS
+   */
+  css: ["@/assets/css/global.css"],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
-  modules: [['@nuxtjs/dotenv', { systemvars: true }]],
+   ** Nuxt.js modules
+   */
+  modules: [["@nuxtjs/dotenv", { systemvars: true }]],
 
   generate: {
     routes: async function() {
-      const homepageData = await queryForDocType('homepage')
-      const aboutPageData = await queryForDocType('about_page')
-      const blogPostsData = await queryForDocType('blog_post')
+      const homepageData = await queryForDocType("homepage");
+      const aboutPageData = await queryForDocType("about_page");
+      const blogPostsData = await queryForDocType("blog_post");
 
-      const blogPosts = blogPostsData.results.map(payload => ({ 
-        route: `/blog/${payload.uid}`, 
-        payload 
-      }))
+      const blogPosts = blogPostsData.results.map(payload => ({
+        route: `/blog/${payload.uid}`,
+        payload
+      }));
 
       return [
-        { route: '/', payload: homepageData.results.data },
-        { route: '/about', payload: aboutPageData.results.data },
-        { route: '/blog', payload: blogPostsData.results.data },
+        { route: "/", payload: homepageData.results.data },
+        { route: "/about", payload: aboutPageData.results.data },
+        { route: "/blog", payload: blogPostsData.results.data },
         ...blogPosts
-      ]
+      ];
     }
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
